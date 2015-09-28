1c7bbd7ecbf547a96ad3dc3e0b4c612e9ca9e1ea
Hector Jose Flores Colmenarez <hecto932@gmail.com>
2015-09-28 12:40:17 -0430

    Update blacklist_c.h

--- shield/blacklist_c.h
+++ shield/blacklist_c.h
@@ -1,3 +1,5 @@
+//REFERENCE : https://github.com/mjnaderi/Sharif-Judge/blob/7ca3101bb848447e8d8aabb14aae0d6ae9dfde31/tester/shield/defc.h
+
 /*
  SI DESEA AGREGAR UNA NUEVA DESCRIPCCION
  SOLO COLOQUELA DEBAJO DE LAS LINEAS "#define"
