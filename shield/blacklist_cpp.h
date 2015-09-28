850c1ad17668f2bab72a43e9c5f481735fa4ebe9
Hector Jose Flores Colmenarez <hecto932@gmail.com>
2015-09-28 12:34:13 -0430

    Documentación.

--- shield/blacklist_cpp.h
+++ shield/blacklist_cpp.h
@@ -1,11 +1,8 @@
-/*****************************************
- * FILE: def_c.h                         *
- * AUTOR: Hector Jose Flores Colmenarez  *
- * EMAIL: hecto932@gmail.com             *           
+/*          
  *****************************************
  SI DESEA AGREGAR UNA NUEVA DESCRIPCCION
  SOLO COLOQUELA DEBAJO DE LAS LINEAS "#define"
- ** HAY FUNCIONES EN C EQUE EN C++ NO FUNCIONAN
+ ** HAY FUNCIONES EN C QUE EN C++ NO FUNCIONAN
 */
 
 #define fork errorNo1       //Fork is not allowed
