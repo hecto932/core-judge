df9706eb8c2c01033f7b6ab2ba88ed7f8cf37d2f
Hector Jose Flores Colmenarez <hecto932@gmail.com>
2015-09-28 12:41:33 -0430

    Update shield_cpp.cpp

--- shield/shield_cpp.cpp
+++ shield/shield_cpp.cpp
@@ -1,3 +1,4 @@
+//REFERENCE : https://github.com/mjnaderi/Sharif-Judge/blob/7ca3101bb848447e8d8aabb14aae0d6ae9dfde31/tester/shield/defcpp.h
 /*
  SI DESEA AGREGAR UNA NUEVA DESCRIPCCION
  SOLO COLOQUELA DEBAJO DE LAS LINEAS "#define"
